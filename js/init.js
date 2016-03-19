75379219696631ed51c43677742f864c6a74eea3
josenaldo <josenaldo@gmail.com>
2016-03-19 04:56:42 -0300

    Tomo 4

--- js/init.js
+++ js/init.js
@@ -1,33 +1,39 @@
-$( document ).ready(function() {
-   Reveal.initialize({
+( function () {
+    'use strict';
+
+    Reveal.initialize( {
+        width: '90%',
+        height: '100%',
         controls: true,
         progress: true,
         history: true,
         center: true,
-        transition: 'slide', // none/fade/slide/convex/concave/zoom
+        slideNumber: true,
+        theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+        transition: Reveal.getQueryHash().transition || 'slide', // none/fade/slide/convex/concave/zoom
         // Optional reveal.js plugins
-        dependencies: [{
+        dependencies: [ {
             src: 'lib/js/classList.js',
-            condition: function() {
+            condition: function () {
                 return !document.body.classList;
             }
         }, {
             src: 'plugin/markdown/marked.js',
-            condition: function() {
-                return !!document.querySelector('[data-markdown]');
+            condition: function () {
+                return !!document.querySelector( '[data-markdown]' );
             }
         }, {
             src: 'plugin/markdown/markdown.js',
-            condition: function() {
-                return !!document.querySelector('[data-markdown]');
+            condition: function () {
+                return !!document.querySelector( '[data-markdown]' );
             }
         }, {
             src: 'plugin/highlight/highlight.js',
             async: true,
-            condition: function() {
-                return !!document.querySelector('pre code');
+            condition: function () {
+                return !!document.querySelector( 'pre code' );
             },
-            callback: function() {
+            callback: function () {
                 hljs.initHighlightingOnLoad();
             }
         }, {
@@ -36,6 +42,7 @@ $( document ).ready(function() {
         }, {
             src: 'plugin/notes/notes.js',
             async: true
-        }]
-    });
-});
+        } ]
+    } );
+
+} )();
